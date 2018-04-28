7db8b5a2c3a78705290b9f8a8e3db6740a71f84a
Oliver Chapman <O.Chapman@2014.hull.ac.uk>
2018-04-28 16:17:00 +0100

    recommit on new git

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ILogger _logger;
+
+        public HomeController(
+            UserManager<ApplicationUser> userManager,
+            ILogger<AccountController> logger)
+        {
+            _userManager = userManager;
+            _logger = logger;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return View();
+            }
+            if (await _userManager.IsInRoleAsync(user, "Demonstrator"))
+            {
+                _logger.LogInformation("User is Demonstrator.");
+                return RedirectToAction(nameof(DemonstratorController.Index), "Demonstrator");
+            }
+            if (await _userManager.IsInRoleAsync(user, "Student"))
+            {
+                _logger.LogInformation("User is Student.");
+                return RedirectToAction(nameof(StudentController.Index), "Student");
+            }
+
+            throw new Exception("User has no Role");
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplication1/Models/ManageViewModels/IndexViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models.ManageViewModels
+{
+    public class IndexViewModel
+    {
+        [Display(Name = "Student Number")]
+        public string Username { get; set; }
+
+        public bool IsEmailConfirmed { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Forename")]
+        public string Forename { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Surname")]
+        public string Surname { get; set; }
+
+
+        public string StatusMessage { get; set; }
+    }
+}
