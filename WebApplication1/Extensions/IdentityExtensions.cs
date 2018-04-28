2ae5ca041bc2e0b1ca5062b24193544aa55b2b38
Oliver Chapman <O.Chapman@2014.hull.ac.uk>
2018-04-28 17:04:06 +0100

    Revert "Revert "recommit on new git""
    This reverts commit ad1a85dda9b8fe77c5162b1a2c55eb0abc01a0a6.

---
+++ WebApplication1/Extensions/IdentityExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+
+namespace WebApplication1.Extensions
+{
+	public static class IdentityExtensions
+	{
+	}
+}
