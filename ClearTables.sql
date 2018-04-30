dbba848a4e0f8da66e8dbafb8eb218b04f50a561
OliverChapman <O.Chapman@2014.hull.ac.uk>
2018-04-30 18:05:19 +0100

    Revert "Revert "Revert "recommit on new git"""
    This reverts commit 1d8082733f0c296a055ce3bbce90335a802245ac.

--- ClearTables.sql
+++
@@ -1,7 +0,0 @@
-﻿DELETE FROM dbo.AspNetRoleClaims
-DELETE FROM dbo.AspNetRoles
-DELETE FROM dbo.AspNetUserClaims
-DELETE FROM dbo.AspNetUserLogins
-DELETE FROM dbo.AspNetUserRoles
-DELETE FROM dbo.AspNetUsers
-DELETE FROM dbo.AspNetUserTokens
